2f20882ae0591398d371f003816b0696d184dff5
Helloworldzz <58582177+ProgramOfRobot@users.noreply.github.com>
2021-05-04 13:55:45 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,41 +1,35 @@
-function Turn_With_Degrees (degrees: number) {
-    if (degrees < 0) {
-        _degress = Math.abs(degrees)
-        Tinybit.CarCtrlSpeed2(Tinybit.CarState.Car_Run, 0, 0)
-    } else {
-    	
-    }
-}
-function Turn_With_90_Degrees (absValue: number) {
-    if (absValue == -1) {
-        Tinybit.CarCtrlSpeed2(Tinybit.CarState.Car_SpinLeft, speet - speet * 2, speet)
-        basic.pause(speet / 4 * 10)
-        Tinybit.CarCtrlSpeed2(Tinybit.CarState.Car_Stop, 0, 0)
-    } else {
-        Tinybit.CarCtrlSpeed2(Tinybit.CarState.Car_SpinRight, speet, speet - speet * 2)
-        basic.pause(speet / 4 * 10)
-        Tinybit.CarCtrlSpeed2(Tinybit.CarState.Car_Run, 0, 0)
-    }
-}
-let _degress = 0
-let speet = 0
-Tinybit.RGB_Car_Big(Tinybit.enColor.Red)
-let step = 44
-speet = 80
-while (true) {
-    Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, speet)
-    if (Tinybit.Ultrasonic_Car() < step) {
-        Turn_With_90_Degrees(-1)
-        if (Tinybit.Ultrasonic_Car() < step) {
-            Turn_With_90_Degrees(1)
-            Turn_With_90_Degrees(1)
-            if (Tinybit.Ultrasonic_Car() < step) {
-                Turn_With_90_Degrees(1)
-                continue;
-            } else {
-                continue;
+function RunLoop_Of_Car () {
+    while (true) {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, speedOfCar)
+        if (Tinybit.Ultrasonic_Car() < distanceOfCSB) {
+            Turn_With_1_Degrees(-1)
+            if (Tinybit.Ultrasonic_Car() < distanceOfCSB) {
+                Turn_With_1_Degrees(1)
+                Turn_With_1_Degrees(1)
+                if (Tinybit.Ultrasonic_Car() < distanceOfCSB) {
+                    Turn_With_1_Degrees(1)
+                }
             }
-            continue;
         }
     }
 }
+function Turn_With_1_Degrees (absValue: number) {
+    if (absValue == -1) {
+        Tinybit.CarCtrlSpeed2(Tinybit.CarState.Car_SpinLeft, speedOfCar - speedOfCar * 2, speedOfCar)
+        basic.pause(speedOfCar / 4 * 10)
+        Tinybit.CarCtrlSpeed2(Tinybit.CarState.Car_Stop, 0, 0)
+    } else {
+        Tinybit.CarCtrlSpeed2(Tinybit.CarState.Car_SpinRight, speedOfCar, speedOfCar - speedOfCar * 2)
+        basic.pause(speedOfCar / 4 * 10)
+        Tinybit.CarCtrlSpeed2(Tinybit.CarState.Car_Run, 0, 0)
+    }
+}
+function Init () {
+    distanceOfCSB = 35
+    speedOfCar = 80
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Red)
+}
+let distanceOfCSB = 0
+let speedOfCar = 0
+Init()
+RunLoop_Of_Car()
